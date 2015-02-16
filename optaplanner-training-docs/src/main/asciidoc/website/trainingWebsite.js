bcfb4d408c805e8b6a713bbc77cdb877cd0c1307
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2015-02-16 14:31:04 +0100

    Expose cheaters

--- optaplanner-training-docs/src/main/asciidoc/website/trainingWebsite.js
+++ optaplanner-training-docs/src/main/asciidoc/website/trainingWebsite.js
@@ -6,11 +6,13 @@ function toggleShowSolutionBlock(solutionBlock) {
 function insertShowSolutionButtons() {
     $('.solution').each(function(idx, node) {
         var solutionBlock = $(node);
-        var showSolutionButton = $('<div><a class="show-solution" href="#">Show solution</a></div>');
+        var showSolutionButton = $('<a class="show-solution" href="#">Show solution</a>');
         solutionBlock.prev().append(showSolutionButton);
         showSolutionButton.on('click', function(event) {
             event.preventDefault();
             toggleShowSolutionBlock(solutionBlock);
+            // Expose cheaters (as long as they don't refresh the page)
+            showSolutionButton.text('Show/hide solution again');
         });
         toggleShowSolutionBlock(solutionBlock);
     });
