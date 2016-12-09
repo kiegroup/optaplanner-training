4dfe3ab05c8b9dc1dd9c026393392f11b90ec066
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2016-12-09 14:37:30 +0100

    worker rostering: domain + generator

---
+++ optaplanner-training-lab902-solution/src/main/java/org/optaplanner/training/workerrostering/persistence/StringDataGenerator.java
@@ -0,0 +1,227 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaplanner.training.workerrostering.persistence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringDataGenerator {
+
+    public static StringDataGenerator build10kFullNames() {
+        return new StringDataGenerator()
+                .addPart(
+                        "Amy",
+                        "Beth",
+                        "Chad",
+                        "Dan",
+                        "Elsa",
+                        "Flo",
+                        "Gus",
+                        "Hugo",
+                        "Ivy",
+                        "Jay")
+                .addPart(
+                        "A.",
+                        "B.",
+                        "C.",
+                        "D.",
+                        "E.",
+                        "F.",
+                        "G.",
+                        "H.",
+                        "I.",
+                        "J.")
+                .addPart(
+                        "O.",
+                        "P.",
+                        "Q.",
+                        "R.",
+                        "S.",
+                        "T.",
+                        "U.",
+                        "V.",
+                        "W.",
+                        "X.")
+                .addPart(
+                        "Cole",
+                        "Fox",
+                        "Green",
+                        "Jones",
+                        "King",
+                        "Li",
+                        "Poe",
+                        "Rye",
+                        "Smith",
+                        "Watt");
+    }
+
+    public static StringDataGenerator build1kCompanyNames() {
+        return new StringDataGenerator()
+                .addPart(
+                        "Steel",
+                        "Paper",
+                        "Stone",
+                        "Wood",
+                        "Water",
+                        "Food",
+                        "Oil",
+                        "Car",
+                        "Power",
+                        "Computer")
+                .addPart(
+                        "Inc",
+                        "Corp",
+                        "Limited",
+                        "Express",
+                        "Telco",
+                        "Mobile",
+                        "Soft",
+                        "Mart",
+                        "Bank",
+                        "Labs")
+                .addPart(
+                        "US",
+                        "UK",
+                        "JP",
+                        "DE",
+                        "FR",
+                        "BE",
+                        "NL",
+                        "BR",
+                        "IN",
+                        "ES");
+    }
+
+    public static StringDataGenerator build10kLocationNames() {
+        return new StringDataGenerator()
+                .addPart(
+                        "Los",
+                        "San",
+                        "Las",
+                        "Santa",
+                        "New",
+                        "Fort",
+                        "Mount",
+                        "Saint",
+                        "Little",
+                        "El")
+                .addPart(
+                        "Angeles",
+                        "Francisco",
+                        "Vegas",
+                        "Barbara",
+                        "York",
+                        "Collins",
+                        "Vernon",
+                        "Peter",
+                        "Rock",
+                        "Paso")
+                .addPart(
+                        "Town",
+                        "City",
+                        "Falls",
+                        "Ville",
+                        "Field",
+                        "Polis",
+                        "Shire",
+                        "Berg",
+                        "Borough",
+                        "Island")
+                .addPart(
+                        "AL",
+                        "CA",
+                        "DE",
+                        "FL",
+                        "GA",
+                        "HI",
+                        "IN",
+                        "KS",
+                        "LA",
+                        "ME");
+    }
+
+    private final String delimiter;
+    private List<String[]> partValuesList = new ArrayList<>();
+    private int partValuesLength;
+    private int index = 0;
+    private int indexLimit;
+
+    public StringDataGenerator() {
+        this(" ");
+    }
+
+    public StringDataGenerator(String delimiter) {
+        this.delimiter = delimiter;
+    }
+
+    public StringDataGenerator addPart(String... partValues) {
+        if (partValuesList.isEmpty()) {
+            partValuesLength = partValues.length;
+        } else {
+            if (partValues.length != partValuesLength) {
+                throw new IllegalStateException("The partValues length (" + partValues.length
+                        + ") is not the same as the partValuesLength (" + partValuesLength + ") of the others.");
+            }
+        }
+        partValuesList.add(partValues);
+        indexLimit = (int) Math.pow(partValuesLength, partValuesList.size());
+        return this;
+    }
+
+    /**
+     * Do  not scroll per digit (0000, 1111, 2222, 0001, 1112, 2220, 0002, 1110, 2221, ...)
+     * Instead, scroll per half (0000, 1111, 2222, 0011, 1122, 2200, 0022, 1100, 2211, ...)
+     */
+    private int[][] halfSequenceMap = new int[][]{{}, {0}, {0, 1}, {0, 2, 1}, {0, 2, 1, 3}};
+
+    public String generateNextValue() {
+        if (index >= indexLimit) {
+            throw new IllegalStateException("No more elements: the index (" + index + ") is too high.");
+        }
+        int listSize = partValuesList.size();
+        StringBuilder result = new StringBuilder(listSize * 80);
+        // Make sure we have a unique combination
+        if (listSize >= halfSequenceMap.length) {
+            throw new IllegalStateException("A listSize (" + listSize + ") is not yet supported.");
+        }
+        int[] halfSequence = halfSequenceMap[listSize];
+        int[] chosens = new int[listSize];
+        int previousChosen = 0;
+        for (int i = 0; i < listSize; i++) {
+            int chosen = (previousChosen
+                    + (index % (int) Math.pow(partValuesLength, halfSequence[i] + 1)
+                            / (int) Math.pow(partValuesLength, halfSequence[i])))
+                    % partValuesLength;
+            chosens[i] = chosen;
+            previousChosen = chosen;
+        }
+        for (int i = 0; i < listSize; i++) {
+            if (i > 0) {
+                result.append(delimiter);
+            }
+            String[] partValues = partValuesList.get(i);
+            result.append(partValues[chosens[i]]);
+        }
+        index++;
+        return result.toString();
+    }
+
+    public void reset() {
+        index = 0;
+    }
+
+}
---
+++ optaplanner-training-lab902-solution/src/main/java/org/optaplanner/training/workerrostering/persistence/WorkerRosteringGenerator.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.optaplanner.training.workerrostering.persistence;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Random;
+
+import org.optaplanner.training.workerrostering.domain.Employee;
+import org.optaplanner.training.workerrostering.domain.Roster;
+import org.optaplanner.training.workerrostering.domain.RosterParametrization;
+import org.optaplanner.training.workerrostering.domain.ShiftAssignment;
+import org.optaplanner.training.workerrostering.domain.Skill;
+import org.optaplanner.training.workerrostering.domain.Spot;
+import org.optaplanner.training.workerrostering.domain.TimeSlot;
+
+public class WorkerRosteringGenerator {
+
+    private static final StringDataGenerator employeeNameGenerator = StringDataGenerator.build10kFullNames();
+    private static final StringDataGenerator spotNameGenerator = StringDataGenerator.build10kLocationNames();
+
+    private static final StringDataGenerator skillNameGenerator = new StringDataGenerator()
+            .addPart(
+                    "Mechanical",
+                    "Electrical",
+                    "Safety",
+                    "Transportation",
+                    "Operational",
+                    "Physics",
+                    "Monitoring",
+                    "ICT")
+            .addPart(
+                    "bachelor",
+                    "engineer",
+                    "instructor",
+                    "coordinator",
+                    "manager",
+                    "expert",
+                    "inspector",
+                    "analyst");
+
+    public static void main(String[] args) {
+        WorkerRosteringGenerator generator = new WorkerRosteringGenerator();
+        Roster roster = generator.createRoster(10, 28);
+
+    }
+
+    protected Random random;
+
+    public WorkerRosteringGenerator() {
+        random = new Random(37);
+    }
+
+    public Roster createRoster(int spotListSize, int timeSlotListSize) {
+        int employeeListSize = spotListSize * 4;
+        int skillListSize = (spotListSize + 4) / 5;
+        RosterParametrization rosterParametrization = new RosterParametrization();
+        List<Skill> skillList = createSkillList(skillListSize);
+        List<Spot> spotList = createSpotList(spotListSize, skillList);
+        List<TimeSlot> timeSlotList = createTimeSlotList(timeSlotListSize);
+        List<Employee> employeeList = createEmployeeList(employeeListSize, skillList);
+        List<ShiftAssignment> shiftAssignmentList = createShiftAssignmentList(spotList, timeSlotList, employeeList);
+        return new Roster(rosterParametrization,
+                skillList, spotList, timeSlotList, employeeList,
+                shiftAssignmentList);
+    }
+
+    private List<Skill> createSkillList(int size) {
+        List<Skill> skillList = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            String name = skillNameGenerator.generateNextValue();
+            skillList.add(new Skill(name));
+        }
+        return skillList;
+    }
+
+    private List<Spot> createSpotList(int size, List<Skill> skillList) {
+        List<Spot> spotList = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            String name = spotNameGenerator.generateNextValue();
+            spotList.add(new Spot(name, skillList.get(random.nextInt(skillList.size()))));
+        }
+        return spotList;
+    }
+
+    private List<TimeSlot> createTimeSlotList(int size) {
+        List<TimeSlot> timeSlotList = new ArrayList<>(size);
+        LocalDateTime previousEndDateTime = LocalDateTime.of(2017, 2, 1, 6, 0);
+        for (int i = 0; i < size; i++) {
+            LocalDateTime startDateTime = previousEndDateTime;
+            LocalDateTime endDateTime = startDateTime.plusHours(8);
+            timeSlotList.add(new TimeSlot(startDateTime, endDateTime));
+            previousEndDateTime = endDateTime;
+        }
+        return timeSlotList;
+    }
+
+    private List<Employee> createEmployeeList(int size, List<Skill> generalSkillList) {
+        List<Employee> employeeList = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            String name = employeeNameGenerator.generateNextValue();
+            LinkedHashSet<Skill> skillSet = new LinkedHashSet<>(extractRandomSubList(generalSkillList));
+            employeeList.add(new Employee(name, skillSet));
+        }
+        return employeeList;
+    }
+
+    private List<ShiftAssignment> createShiftAssignmentList(List<Spot> spotList, List<TimeSlot> timeSlotList,
+            List<Employee> employeeList ) {
+        List<ShiftAssignment> shiftAssignmentList = new ArrayList<>(spotList.size() * timeSlotList.size());
+        for (Spot spot : spotList) {
+            boolean weekendEnabled = random.nextInt(10) < 8;
+            boolean nightEnabled = weekendEnabled && random.nextInt(10) < 8;
+            for (TimeSlot timeSlot : timeSlotList) {
+                DayOfWeek dayOfWeek = timeSlot.getStartDateTime().getDayOfWeek();
+                if (!weekendEnabled && (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY)) {
+                    continue;
+                }
+                if (!nightEnabled && timeSlot.getStartDateTime().getHour() >= 20) {
+                    continue;
+                }
+                shiftAssignmentList.add(new ShiftAssignment(spot, timeSlot));
+            }
+
+        }
+        return shiftAssignmentList;
+
+    }
+
+    private <E> List<E> extractRandomSubList(List<E> list) {
+        List<E> subList = new ArrayList<>(list);
+        Collections.shuffle(subList, random);
+        // TODO List.subList() doesn't allow outer list to be garbage collected
+        return subList.subList(0, random.nextInt(list.size()) + 1);
+    }
+
+}
