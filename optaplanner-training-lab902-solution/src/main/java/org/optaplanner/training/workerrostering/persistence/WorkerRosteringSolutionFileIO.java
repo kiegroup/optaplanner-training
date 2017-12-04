a8820e891cf9d2967d1bc71bb16d067262c692ee
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2017-12-04 13:32:43 +0100

    code style

--- optaplanner-training-lab902-solution/src/main/java/org/optaplanner/training/workerrostering/persistence/WorkerRosteringSolutionFileIO.java
+++ optaplanner-training-lab902-solution/src/main/java/org/optaplanner/training/workerrostering/persistence/WorkerRosteringSolutionFileIO.java
@@ -93,7 +93,7 @@ public class WorkerRosteringSolutionFileIO implements SolutionFileIO<Roster> {
         }
     }
 
-    private static  class RosterReader {
+    private static class RosterReader {
 
         private final Workbook workbook;
 
